752d6ae00e455112a93b41aade8dc8c8766d536c
Gian Vallejos <gian.vallejos92@gmail.com>
2024-09-17 17:16:03 -0500

    Add, edit fields
    Signed-off-by: Gian Vallejos <gian.vallejos92@gmail.com>

--- server/src/routes/fields.routes.js
+++ server/src/routes/fields.routes.js
@@ -21,7 +21,7 @@ router.get('/field/list/:objectId', async(req, res) => {
 router.get('/field/add/:objectId', async(req, res) => {
   try {
     const {objectId} = req.params;
-    res.render('fields/add');
+    res.render('fields/add', {objectId: objectId});
   } catch (err) {
     res.status(500).json({
       message: err.message
@@ -29,17 +29,53 @@ router.get('/field/add/:objectId', async(req, res) => {
   }
 });
 
-router.post('/field/add', async(req, res) => {
+router.post('/field/add/:objectId', async(req, res) => {
   try {
-    const {Name, API_Name} = req.body;
+    const {objectId} = req.params;
+    const {Name, API_Name, type, required} = req.body;
+    let isRequired = (required === 'on') ? true : false;
     const newObject = {
-      Name, API_Name
+      Name, API_Name, type, isRequired, objectId
     }
     
-    //Validate unique API_name in all databases 
+    await pool.query('INSERT INTO field SET ?', [newObject]); //call insertField Procedure
+    res.redirect('/field/list/' + objectId);
+
+  } catch (err) {
+    res.status(500).json({
+      message: err.message
+    });
+  }
+});
+
+//Edit Object
+router.get('/field/edit/:objectId/:id', async(req, res) => {
+  try {
+    const {id, objectId} = req.params;
+    const [field] = await pool.query('SELECT id, Name, API_Name, isRequired FROM field WHERE id = ?', [id]); //call procedure
+    const fieldEdit = field[0];
+
+    res.render('fields/edit', { field: fieldEdit, fieldId: id, objectId });
+
+  } catch (err) {
+    res.status(500).json({
+      message: err.message
+    });
+  }
+});
+
+router.post('/field/edit/:objectId/:id', async(req, res) => {
+  try {
+    const {Name, API_Name, type, required} = req.body;
+    const {id, objectId} = req.params;
+    let isRequired = (required === 'on') ? true : false;
+    const newObject = {
+      Name, API_Name, type, isRequired
+    }
+
+    await pool.query('UPDATE field SET ? WHERE id = ?', [newObject, id]); //call procedure
     
-    await pool.query('INSERT INTO Object SET ?', [newObject]); //call insertObject Procedure
-    res.redirect('/field/list/:id');
+    res.redirect('/field/list/' + objectId);
 
   } catch (err) {
     res.status(500).json({
