42e95ada259dad00c6d5b323b68a3e34e49273e9
Gian Vallejos <gian.vallejos92@gmail.com>
2024-09-17 15:03:47 -0500

    add field
    Signed-off-by: Gian Vallejos <gian.vallejos92@gmail.com>

--- server/src/routes/fields.routes.js
+++ server/src/routes/fields.routes.js
@@ -7,11 +7,40 @@ router.get('/field/list/:objectId', async(req, res) => {
   try {
     const {objectId} = req.params;
     const [result] = await pool.query('SELECT * FROM field WHERE objectId = ? LIMIT 1000', [objectId]);
-    /*
-    res.status(200).json({
-      fields: result
-    });*/
-    res.render('fields/list', {fields: result});
+    const [objectName] = await pool.query('SELECT Name as objectName FROM OBJECT WHERE Id = ? LIMIT 1', [objectId]);
+    
+    res.render('fields/list', {fields: result, objectName: objectName[0].objectName, objectId});
+  } catch (err) {
+    res.status(500).json({
+      message: err.message
+    });
+  }
+});
+
+//Add Field
+router.get('/field/add/:objectId', async(req, res) => {
+  try {
+    const {objectId} = req.params;
+    res.render('fields/add');
+  } catch (err) {
+    res.status(500).json({
+      message: err.message
+    });
+  }
+});
+
+router.post('/field/add', async(req, res) => {
+  try {
+    const {Name, API_Name} = req.body;
+    const newObject = {
+      Name, API_Name
+    }
+    
+    //Validate unique API_name in all databases 
+    
+    await pool.query('INSERT INTO Object SET ?', [newObject]); //call insertObject Procedure
+    res.redirect('/field/list/:id');
+
   } catch (err) {
     res.status(500).json({
       message: err.message
